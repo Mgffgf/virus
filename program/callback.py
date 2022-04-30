969cc6789e142c2c0fdecee7cd2bcfeac473fa8c
Mgffgf <104373753+Mgffgf@users.noreply.github.com>
2022-04-30 05:53:39 +0200

    Add files via upload

---
+++ program/callback.py
@@ -0,0 +1,248 @@
+# Copyright (C) 2021 By AmortMusicProject
+
+from driver.queues import QUEUE
+from pyrogram import Client, filters
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from config import (
+    ASSISTANT_NAME,
+    BOT_NAME,
+    BOT_USERNAME,
+    GROUP_SUPPORT,
+    OWNER_NAME,
+    UPDATES_CHANNEL,
+)
+
+
+@Client.on_callback_query(filters.regex("cbstart"))
+async def cbstart(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""✨ **مرحبآ عزيزي↤「 [{query.message.chat.first_name}](tg://user?id={query.message.chat.id}) 」!**\n
+━━━━━━━━🍁━━━━━━━━
+🎻| **انا بوت تشغيل وتنزيل الاغاني وفديو كول **
+👮🏼‍♂️| ** اضفني مشرف في مجموعتك لأعمل **
+😎| ** اتبع مايلي لمعرفه كيفيه الاستخدام **
+❓| ** اضغط علي ذر طريقه الاستخدام **
+🌐 ** الحساب المساعد  @{ASSISTANT_NAME} **
+◍مـبــرمـج السـورس◍ **[✯𝐌𝐒✯ 𝒗𝒊𝒓𝒖𝒔┋✘🇨🇦!](https://t.me/FLASH_MASR) **
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "✅اضغط لأضافه البوت لمجموعتك✅",
+                        url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                    )
+                ],
+                [InlineKeyboardButton("❓طــريــقــة الاســتخــدام", callback_data="cbhowtouse")],
+                [InlineKeyboardButton("🌀الاوامــر الكامله المعربــه🌀", callback_data="cbvamp")],                
+                [
+                    InlineKeyboardButton("📚الاوامــــر", callback_data="cbcmds"),
+                    InlineKeyboardButton("👨🏼‍💻الــمطــور👨🏼‍💻", url=f"https://t.me/{OWNER_NAME}"),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "🎙جــروب الــدعـم🎙", url=f"https://t.me/{GROUP_SUPPORT}"
+                    ),
+                    InlineKeyboardButton(
+                        "𝗦𝗢𝗨𝗥𝗖𝗘 𝗠𝗔𝗘𝗦𝗧𝗥𝗢┋✘🇨🇦!", url=f"https://t.me/APP_YOUTUBE"
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "✯𝐌𝐒✯ 𝒅𝒆𝒔𝒉𝒂┋✘🇨🇦!", url="https://t.me/J3_X3"
+                    )
+                ],
+            ]
+        ),
+        disable_web_page_preview=True,
+    )
+
+
+@Client.on_callback_query(filters.regex("cbhowtouse"))
+async def cbguides(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f""" الدليل الأساسي لاستخدام هذا البوت:
+
+ 1 ↤ أولاً ، أضفني إلى مجموعتك
+ 2 ↤ بعد ذلك ، قم بترقيتي كمشرف ومنح جميع الصلاحيات باستثناء الوضع الخفي
+ 3 ↤ بعد ترقيتي ، اكتب «تحديث» او /reload مجموعة لتحديث بيانات المشرفين
+ 3 ↤ أضف  @{ASSISTANT_NAME} إلى مجموعتك أو اكتب او «انضم»  /userbotjoin لدعوة حساب المساعد
+ 4 ↤ قم بتشغيل المكالمة  أولاً قبل البدء في تشغيل الفيديو / الموسيقى
+ 5 ↤ في بعض الأحيان ، يمكن أن تساعدك إعادة تحميل البوت باستخدام الأمر «تحديث» او /reload في إصلاح بعض المشكلات
+ 📌 إذا لم ينضم البوت إلى المكالمة ، فتأكد من تشغيل المكالمة  بالفعل ، أو اكتب «غادر» /userbotleave ثم اكتب «انضم» او /userbotjoin مرة أخرى
+
+ 💡 إذا كانت لديك أسئلة  حول هذا البوت ، فيمكنك إخبارنا منن خلال قروب الدعم الخاصة بي هنا ↤ @{GROUP_SUPPORT}
+
+⚡🎶قناة البوت @APP_YOUTUBE
+__""",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("🔙 رجوع", callback_data="cbstart")]]
+        ),
+    )
+
+
+@Client.on_callback_query(filters.regex("cbcmds"))
+async def cbcmds(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""✨ **Hello [{query.message.chat.first_name}](tg://user?id={query.message.chat.id}) !**
+
+» **⇦قم بالضغط علي الزر الذي تريده لمعرفه الاوامر لكل فئه منهم !**
+
+⚡ __مطور البوت»  @{UPDATES_CHANNEL}  __""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("اوامــر المشــرف🙋🏼‍♂️", callback_data="cbadmin"),
+                    InlineKeyboardButton("👨🏼‍💻اوامــر المطــور👨🏼‍💻", callback_data="cbsudo"),
+                ],[
+                    InlineKeyboardButton("🎮الاوامــر الكامله المعربــه🎮", callback_data="cbvamp")                    
+                ],[
+                    InlineKeyboardButton("🔙 رجوع", callback_data="cbstart")
+                ],
+            ]
+        ),
+    )
+
+
+@Client.on_callback_query(filters.regex("cbbasic"))
+async def cbbasic(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""🏮 ها هي الأوامر الاساسية:
+ملحوظه الاوامر المعربه تكتب كما هي بدون شرط او اي شيء» /mplayاو «تشغيل» 「اسم الأغنية / رابط」تشغيل الصوت mp3
+» /vplay او «فديو» 「اسم / رابط الفيديو」 تشغيل الفيديو داخل المكالمة 
+» /stream او«تشغيل» 「رابط 」تشغيل صوت
+» /vstream او «فديو» 「رابط」 تشغيل فيديو مباشر من اليوتيوب
+» /stop او «ايقاف» لايقاف التشغيل
+» /resume «او لاستئناف التشغيل«مواصله  
+» /skip او «تقدم» تخطي الئ التالي
+» /pauseاو «وقف» ايقاف التشغيل موقتآ
+» /vmute «لكتم البوت او «كتم
+» /vunmute«او «الغاء الكتم لرفع الكتم عن البوت
+⚡🎶قناة البوت @APP_YOUTUBE
+__""",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("🔙رجوع", callback_data="cbcmds")]]
+        ),
+    )
+
+
+@Client.on_callback_query(filters.regex("cbadmin"))
+async def cbadmin(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""  »
+ » /playlist  او «تحكم» ↤ تظهر لك قائمة التشغيل
+ » /videoاو «تنزيل» + الاسم  تنزيل فيديو من youtube
+ » /song +  او« تحميل» الاسم تنزيل صوت من youtube
+» /volume  او «الصوت»+ الرقم لضبط مستوئ الصوت
+» /reload  او «تحديث» لتحديث البوت و قائمة المشرفين
+» /userbotjoin  او «انضم» لاستدعاء حساب المساعد
+» /userbotleave  او «غادر» لطرد حساب المساعد 
+ » /pingاو«تيست» - إظهار حالة البوت بينغ
+ » /alive   او «السورس» إظهار معلومات البوت  (في المجموعه) 
+⚡🎶قناة البوت @APP_YOUTUBE
+__""",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("🔙 رجوع", callback_data="cbcmds")]]
+        ),
+    )
+
+@Client.on_callback_query(filters.regex("cbsudo"))
+async def cbsudo(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""🏮 here is the sudo commands:
+
+» /rmw  »او «مسح- clean all raw files
+» /rmd  » او «تنظيف- clean all downloaded files
+» /sysinfo»او «معلومات- show the system information
+» /update»او «ترقيه - update your bot to latest version
+» /restart «او «تنصيب - restart your bot
+» /leaveall»او «غادرالجميع - order userbot to leave from all group
+
+⚡🎶قناة البوت @APP_YOUTUBE
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("🔙رجوع", callback_data="cbcmds")]]
+        ),
+    )
+
+@Client.on_callback_query(filters.regex("cbvamp"))
+async def cbbasic(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""🌀 ها هي الاوامر المعربه الكامله :
+الاوامر المعربه تكتب كما هي بدون شرط او اي شيء
+━━━━━━━━━━━━
+⇦اوامر تشغيل البوت في المجموعات⇨
+⇦ ✪『  تشغيل 』✪➢ ➕ 「اسم الأغنية او / رابط」تشغيل الصوت mp3
+⇦ ✪『  فديو 』✪➢ ➕ 「اسم الفديو او / رابط الفيديو」 تشغيل الفيديو داخل المكالمة 
+⇦ ✪『  تشغيل 』✪➢ ➕  「رابط 」تشغيل صوت
+ ⇦ ✪『  فديو 』✪➢ ➕  「رابط」 تشغيل فيديو مباشر من اليوتيوب
+⇦ ✪『 ايقاف او انهاء』✪➢ ☆ لايقاف التشغيل
+⇦ ✪『  وقف 』✪➢ ☆ ايقاف التشغيل موقتآ 
+⇦ ✪『 تقدم 』✪➢ ☆ تخطي الئ التالي
+⇦ ✪『   مواصله  』✪➢ ☆ استئناف التشغيل 
+⇦ ✪『   كتم او سكوت 』✪➢ ☆ لكتم البوت
+⇦ ✪『 الغاء الكتم』✪➢ ☆ لرفع كتم البوت
+━━━━━━━━━━━━
+⇦اوامر التحكم بلبوت خارج وداخل المجموعات⇨
+⇦ ✪『   تحكم 』✪➢ ☆ ↤ تظهر لك قائمة التشغيل
+⇦ ✪『   تنزيل 』✪➢ ➕ «اسم الفديو» لتنزيل فديو من اليوتيوب 
+⇦ ✪『   تحميل 』✪➢ ➕ «اسم الاغنيه» لتحميل اغنيه من اليوتيوب 
+⇦ ✪『   بحث 』✪➢ ➕ «اي شيء تريد البحث عنه بليوتيوب»
+⇦ ✪『   الصوت 』✪➢ ➕ «كتابه» الرقم لضبط مستوئ الصوت
+⇦ ✪『   تحديث 』✪➢ ☆ لتحديث البوت و قائمة المشرفين
+⇦ ✪『   انضم 』✪➢ ☆ لاستدعاء حساب المساعد
+⇦ ✪『   غادر 』✪➢ ☆ لطرد حساب المساعد 
+⇦ ✪『   تيست 』✪➢ ☆ - إظهار حالة البوت بينغ
+⇦ ✪『   الوقت 』✪➢ ☆ - اظهار الوقت
+⇦ ✪『   السورس 』✪➢ ☆  إظهار معلومات البوت  (في المجموعة)
+━━━━━━━━━━━━
+⇦اوامر تحكم المطور⇨
+⇦ ✪『   مسح  』✪➢ ☆ لمسح جميع الملفات المستخدمه
+⇦ ✪『   تنظيف  』✪➢ ☆ لتنظيف جميع الملفات المحمله
+⇦ ✪『   معلومات  』✪➢ ☆ لرؤيه معلومات النظام 
+⇦ ✪『  ترقيه 』✪➢ ☆ لترقيه البوت لاخر اصدار من السورس
+⇦ ✪『  تنصيب 』✪➢ ☆ لاعاده التشغيل من هيركو
+⇦ ✪『  غادرالجميع 』✪➢ ☆ لمغادره الحساب المساعد لجميع الدردشات
+━━━━━━━━━━━━━━
+⚡🎶قناة البوت @APP_YOUTUBE
+__""",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("🔙رجوع", callback_data="cbcmds")]]
+        ),
+    )
+           
+
+@Client.on_callback_query(filters.regex("cbmenu"))
+async def cbmenu(_, query: CallbackQuery):
+    if query.message.sender_chat:
+        return await query.answer("you're an Anonymous Admin !\n\n» revert back to user account from admin rights.")
+    a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
+    if not a.can_manage_voice_chats:
+        return await query.answer("💡 المسؤول الوحيد الذي لديه إذن إدارة الدردشات الصوتية يمكنه النقر على هذا الزر !", show_alert=True)
+    chat_id = query.message.chat.id
+    if chat_id in QUEUE:
+          await query.edit_message_text(
+              f"⚙️ **الإعدادات** {query.message.chat.title}\n\n⏸ : ايقاف التشغيل موقتآ\n▶️ : استئناف التشغيل\n🔇 : كتم الصوت\n🔊 : الغاء كتم الصوت\n⏹ : ايقاف التشغيل",
+              reply_markup=InlineKeyboardMarkup(
+                  [[
+                      InlineKeyboardButton("⏹", callback_data="cbstop"),
+                      InlineKeyboardButton("⏸", callback_data="cbpause"),
+                      InlineKeyboardButton("▶️", callback_data="cbresume"),
+                  ],[
+                      InlineKeyboardButton("🔇", callback_data="cbmute"),
+                      InlineKeyboardButton("🔊", callback_data="cbunmute"),
+                  ],[
+                      InlineKeyboardButton("🗑 اغلاق", callback_data="cls")],
+                  ]
+             ),
+         )
+    else:
+        await query.answer("❌ قائمة التشغيل فارغه", show_alert=True)
+
+
+@Client.on_callback_query(filters.regex("cls"))
+async def close(_, query: CallbackQuery):
+    a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
+    if not a.can_manage_voice_chats:
+        return await query.answer("💡 المسؤول الوحيد الذي لديه إذن إدارة الدردشات الصوتية يمكنه النقر على هذا الزر !", show_alert=True)
+    await query.message.delete()
---
+++ program/start.py
@@ -0,0 +1,156 @@
+from datetime import datetime
+from sys import version_info
+from time import time
+
+from config import (
+    ALIVE_IMG,
+    ALIVE_NAME,
+    BOT_NAME,
+    ASSISTANT_NAME,
+    BOT_USERNAME,
+    GROUP_SUPPORT,
+    OWNER_NAME,
+    UPDATES_CHANNEL,
+)
+from program import __version__
+from driver.filters import command, other_filters
+from pyrogram import Client, filters
+from pyrogram import __version__ as pyrover
+from pytgcalls import (__version__ as pytover)
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
+__major__ = 0
+__minor__ = 2
+__micro__ = 1
+
+__python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ("week", 60 * 60 * 24 * 7),
+    ("day", 60 * 60 * 24),
+    ("hour", 60 * 60),
+    ("min", 60),
+    ("sec", 1),
+)
+
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return "inf"
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else "s"))
+    return ", ".join(parts)
+
+
+@Client.on_message(
+    command(["start", f"start@{BOT_USERNAME}"]) & filters.private & ~filters.edited
+)
+async def start_(client: Client, message: Message):
+    await message.reply_text(
+        f"""✨ **مرحبا عزيزي ↤ {message.from_user.mention()} !**\n
+━━━━━━━━🍁━━━━━━━━
+🎻| **انا بوت تشغيل وتنزيل الاغاني وفديو كول **
+👮🏼‍♂️| ** اضفني مشرف في مجموعتك لأعمل **
+😎| ** اتبع مايلي لمعرفه كيفيه الاستخدام **
+❓| ** اضغط علي ذر طريقه الاستخدام **
+🌐 ** الحساب المساعد  @{ASSISTANT_NAME} **
+◍مـبــرمـج السـورس◍ **[✯𝐌𝐒✯ 𝒗𝒊𝒓𝒖𝒔┋✘🇨🇦!](https://t.me/FLASH_MASR) **
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "✅اضغط لأضافه البوت لمجموعتك✅",
+                        url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                    )
+                ],
+                [InlineKeyboardButton("🔮طــريــقــة الاســتخــدام🔮", callback_data="cbhowtouse")],
+                [InlineKeyboardButton("🌀الاوامــر الكامله المعربــه🌀", callback_data="cbvamp")],                 
+                [
+                    InlineKeyboardButton("🎮الاوامــــر🎮", callback_data="cbcmds"),
+                    InlineKeyboardButton("👨🏼‍💻الــمــطــور👨🏼‍💻", url=f"https://t.me/{OWNER_NAME}"),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "🎙جــروب الــدعـم🎙", url=f"https://t.me/{GROUP_SUPPORT}"
+                    ),
+                    InlineKeyboardButton(
+                        "𝗦𝗢𝗨𝗥𝗖𝗘 𝗠𝗔𝗘𝗦𝗧𝗥𝗢┋✘🇨🇦!", url=f"https://t.me/APP_YOUTUBE"
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "✯𝐌𝐒✯ 𝒅𝒆𝒔𝒉𝒂┋✘🇨🇦!", url="https://t.me/J3_X3"
+                    )
+                ],
+            ]
+        ),
+        disable_web_page_preview=True,
+    )
+
+
+@Client.on_message(
+    command(["alive", "لسورس", f"alive@{BOT_USERNAME}"]) & filters.group & ~filters.edited
+)
+async def alive(client: Client, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+
+    keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "✯𝐌𝐒✯ 𝒅𝒆𝒔𝒉𝒂┋✘🇨🇦!,
+                        url=f"https://t.me/J3_X3",
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "🎙جــروب الــدعـم🎙", url=f"https://t.me/{GROUP_SUPPORT}"
+                    ),
+                    InlineKeyboardButton(
+                        "𝐒𝐎𝐔𝐑𝐂𝐄🌀", url=f"https://t.me/{UPDATES_CHANNEL}"
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "🎧اضافه البوت اللي مجموعتك🎧", url="https://t.me/{BOT_USERNAME}?startgroup=true"
+                ),
+            ]
+        ]
+    ) 
+
+    alive = f"**مرحبآ  {message.from_user.mention()}, انا {BOT_NAME}**\n\n✨ البوت يعمل بشكل طبيعي\n👨🏼‍💻المبرمج : [{ALIVE_NAME}](https://t.me/{OWNER_NAME})\n✨ اصدار Bot : `v{__version__}`\n🍀 اصدار Pyrogram : `{pyrover}`\n✨ اصدار Python: `{__python_version__}`\n🍀 اصدار PyTgCalls : `{pytover.__version__}`\n✨ وقت التشغيل: `{uptime}`\n\n**شكرًا لإضافتي هنا ، لتشغيل الفيديو والموسيقى على دردشة الفيديو الخاصة بمجموعتك ** ❤"
+
+    await message.reply_photo(
+        photo=f"{ALIVE_IMG}",
+        caption=alive,
+        reply_markup=keyboard,
+    )
+
+
+@Client.on_message(command(["ping", "يست", f"ping@{BOT_USERNAME}"]) & ~filters.edited)
+async def ping_pong(client: Client, message: Message):
+    start = time()
+    m_reply = await message.reply_text("pinging...")
+    delta_ping = time() - start
+    await m_reply.edit_text("🏓 `PONG!!`\n" f"⚡️ `{delta_ping * 1000:.3f} ms`")
+
+
+@Client.on_message(command(["uptime","لوقت", f"uptime@{BOT_USERNAME}"]) & ~filters.edited)
+async def get_uptime(client: Client, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await message.reply_text(
+        "🤖 bot status:\n"
+        f"• **uptime:** `{uptime}`\n"
+        f"• **start time:** `{START_TIME_ISO}`"
+    )
